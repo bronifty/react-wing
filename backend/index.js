843f1811276b6d5f17234d7222d108822f07285c
bronifty <bronifty@gmail.com>
2023-10-03 16:42:10 -0400

    this

--- backend/index.js
+++ backend/index.js
@@ -62,41 +62,16 @@ app.post("/upload", express.json({ limit: "50mb" }), async (req, res) => {
 });
 
 app.post("/download", async (req, res) => {
-  // const { fileName } = req.body;
-  // console.log(`req.body ${JSON.stringify(req.body, null, 2)}`);
-
   try {
-    // query db for fileName to get key, which is passed to nodeFsReadAndReturn(key)
     const executeDbFunction = await execSQLiteQueryRead(req.body);
     const returnVal = await executeDbFunction();
-    // console.log(
-    //   `back in app.post(/download); just executed 'execSQLiteQueryRead(req.body); returnVal: ${JSON.stringify(
-    //     returnVal,
-    //     null,
-    //     2
-    //   )}`
-    // );
-    // console.log(`returnVal[0]: ${JSON.stringify(returnVal[0], null, 2)}`);
-    // console.log(
-    //   `returnVal[0].key: ${JSON.stringify(returnVal[0].key, null, 2)}`
-    // );
-
-    // res.status(201).json(returnVal);
-    let file = await nodeFsReadAndReturn(returnVal[0].key);
-    // console.log(
-    //   `temp val from nodeFsReadAndReturn ${JSON.stringify(temp, null, 2)}`
-    // );
-
-    // returnVal.file = temp;
-    let payload = { ...returnVal, file };
-    console.log(`returnVal ${payload}`);
-    for (let key in payload) {
-      console.log("for let key in payload");
-
-      console.log(key, payload[key]);
-    }
-
-    res.status(200).send(payload);
+    const updatedReturnVal = await Promise.all(
+      returnVal.map(async (item) => {
+        const base64 = await nodeFsReadAndReturn(item.key);
+        return { ...item, file: base64 };
+      })
+    );
+    res.status(200).send(updatedReturnVal);
   } catch (error) {
     console.log(error);
     res.status(500).send(err);
